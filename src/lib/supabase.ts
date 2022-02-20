0f25ecccb7dedc3893a7ec188ca164d6122563a0
lwood54 <lwood3499@gmail.com>
2022-02-20 00:59:22 -0600

    update and modify fetch override setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(
   import.meta.env.VITE_SB_URL as string,
   import.meta.env.VITE_SB_ANON_KEY as string,
-  { fetch: fetch }
+  {
+    fetch: (...args) => fetch(...args)
+  }
 );
 export default supabase;
